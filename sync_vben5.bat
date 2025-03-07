d58927899f39823907d0ebbb373b2e7ec8c06828
lulifa <814570123@qq.com>
2025-03-07 14:42:19 +0800

    feature：adjust bat cmd file

--- sync_vben5.bat
+++ sync_vben5.bat
@@ -1,15 +1,11 @@
 @echo off
 chcp 65001 > nul
-
-set "REMOTE_NAME=vben5"
-set "REMOTE_URL=https://github.com/vbenjs/vue-vben-admin.git"
-set "BRANCH=main"
-set "SUBDIR=apps/vben5"
+set SUBDIR=apps/vben5
 
 git log -1 --format= -- "%SUBDIR%" 2> nul && goto SYNC
-git remote add %REMOTE_NAME% %REMOTE_URL% 2> nul
-git subtree add --prefix="%SUBDIR%" %REMOTE_NAME% %BRANCH% --squash || exit /b
+git remote add vben5 https://github.com/vbenjs/vue-vben-admin.git 2> nul
+git subtree add --prefix="%SUBDIR%" vben5 main --squash || pause && exit
 
 :SYNC
-git -c core.quotepath=false subtree pull --prefix="%SUBDIR%" %REMOTE_NAME% %BRANCH% --squash
+git -c core.quotepath=false subtree pull --prefix="%SUBDIR%" vben5 main --squash
 echo 操作完成 & pause
