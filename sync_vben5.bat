5dce67453a687322c83846813c159b5f936ff846
lulifa <814570123@qq.com>
2025-03-07 14:05:25 +0800

    feature：adjust bat cmd file

--- sync_vben5.bat
+++ sync_vben5.bat
@@ -1,19 +1,20 @@
 @echo off
 chcp 65001 > nul
-git remote add vben5 https://github.com/vbenjs/vue-vben-admin.git 2> nul
-git log -1 --format='' -- apps/vben5 2> nul
-if %errorlevel% neq 0 (
-    echo 正在初始化子仓库...
-	rmdir /s/q apps\vben5 2> nul
-    git -c core.quotepath=false subtree add --prefix=apps/vben5 vben5 main --squash
-    if %errorlevel% equ 0 (
-        echo 子树初始化成功
-    ) else (
-        echo 初始化失败！代码：%errorlevel%
-        echo 操作建议：删除 apps/vben5 目录后重试
+set SUBDIR=apps/vben5
+
+:: 检测是否已初始化
+git log -1 --format= -- "%SUBDIR%" 2> nul || (
+    echo 首次初始化子仓库...
+    git remote add vben5 https://github.com/vbenjs/vue-vben-admin.git 2> nul
+    git subtree add --prefix="%SUBDIR%" vben5 main --squash || (
+        echo 初始化失败! 错误码: %errorlevel%
+        echo 请手动删除 %SUBDIR% 后重试
+        pause
         exit
     )
 )
-git -c core.quotepath=false subtree pull --prefix=apps/vben5 vben5 main --squash
-if %errorlevel% equ 0 (echo 同步成功！) else (echo 同步失败！代码：%errorlevel% && pause )
+
+:: 执行同步
+git -c core.quotepath=false subtree pull --prefix="%SUBDIR%" vben5 main --squash
+if %errorlevel% equ 0 (echo 同步成功！) else (echo 同步失败！代码：%errorlevel% && pause)
 exit
