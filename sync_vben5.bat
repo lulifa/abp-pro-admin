6aeacd340f4018211aa756e3110b15600027d83f
lulifa <814570123@qq.com>
2025-03-07 10:57:48 +0800

    feature：adjust bat cmd file

--- sync_vben5.bat
+++ sync_vben5.bat
@@ -3,4 +3,4 @@ chcp 65001 > nul
 git remote add vben5 https://github.com/vbenjs/vue-vben-admin.git 2> nul
 git -c core.quotepath=false subtree pull --prefix=apps/vben5 vben5 main --squash
 if %errorlevel% equ 0 (echo 同步成功！) else (echo 同步失败！代码：%errorlevel% && pause )
-exit /b %errorlevel%
+exit
