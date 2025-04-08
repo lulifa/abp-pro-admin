56950a51452f2b83c637b2ec73bd62e84cf64ce5
lulifa <814570123@qq.com>
2025-04-08 14:40:24 +0800

    feature：add localization module domain shared module

---
+++ aspnet-core/modules/localization/RuiChen.AbpPro.LocalizationManagement.Domain.Shared/RuiChen/AbpPro/LocalizationManagement/AbpLocalizationManagementDomainSharedModule.cs
@@ -0,0 +1,28 @@
+﻿using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+using Volo.Abp.VirtualFileSystem;
+
+namespace RuiChen.AbpPro.LocalizationManagement
+{
+    [DependsOn(
+        typeof(AbpValidationModule),
+        typeof(AbpLocalizationModule)
+        )]
+    public class AbpLocalizationManagementDomainSharedModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpLocalizationManagementDomainSharedModule>();
+            });
+
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Resources.Add<LocalizationManagementResource>().AddVirtualJson("/RuiChen/AbpPro/LocalizationManagement/Localization/Resources");
+            });
+
+        }
+    }
+}
---
+++ aspnet-core/modules/localization/RuiChen.AbpPro.LocalizationManagement.Domain.Shared/RuiChen/AbpPro/LocalizationManagement/Features/LocalizationManagementFeatures.cs
@@ -0,0 +1,9 @@
+﻿namespace RuiChen.AbpPro.LocalizationManagement
+{
+    public static class LocalizationManagementFeatures
+    {
+        public const string GroupName = "LocalizationManagement";
+
+        public const string Enable = GroupName + ".Enable";
+    }
+}
