299aa8796650c09a22e2744bd812af22232263ff
lulifa <814570123@qq.com>
2025-05-07 15:16:13 +0800

    feature：add platform entityframework module

---
+++ aspnet-core/modules/platform/RuiChen.Platform.EntityFrameworkCore/RuiChen/Platform/EntityFrameworkCore/PlatformEntityFrameworkCoreModule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Modularity;
+
+namespace RuiChen.Platform
+{
+    [DependsOn(
+        typeof(PlatformDomainModule),
+        typeof(AbpEntityFrameworkCoreModule))]
+    public class PlatformEntityFrameworkCoreModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAbpDbContext<PlatformDbContext>(options =>
+            {
+                options.AddRepository<Data, EfCoreDataRepository>();
+                options.AddRepository<Menu, EfCoreMenuRepository>();
+                options.AddRepository<UserMenu, EfCoreUserMenuRepository>();
+                options.AddRepository<RoleMenu, EfCoreRoleMenuRepository>();
+                options.AddRepository<UserFavoriteMenu, EfCoreUserFavoriteMenuRepository>();
+                options.AddRepository<Layout, EfCoreLayoutRepository>();
+
+                options.AddDefaultRepositories(includeAllEntities: true);
+            });
+        }
+
+    }
+}
